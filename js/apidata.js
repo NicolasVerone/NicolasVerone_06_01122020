645d1c509d8d6f0470f7e816449f4ade401e4b9b
NicolasVerone <nvpro@email.com>
2021-05-04 17:16:48 +0200

    debut import json index

--- js/apidata.js
+++ js/apidata.js
@@ -1,6 +1,22 @@
-const datas = fetch("datajson.json");
+/*const datas = fetch("js/datajson.json");
 datas.then((response) => {
     console.log(response)
 }).catch((error) => {
     console.log(error)
-})
+})*/
+
+const datas = fetch("js/datajson.json")
+.then(response => response.json())
+.then(json => console.log(json))
+
+
+
+    let populateImage = () => {
+        const profileImage = document.getElementById('profile_image');
+        profileImage.src = datas['photographers']
+    
+    };
+
+
+    populateImage();
+
